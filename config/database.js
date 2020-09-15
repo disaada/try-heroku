76b7cefb387db05c4bf7c923770b4467c161fede
Disa A. Syuhada <disaada@gmail.com>
2020-09-15 15:33:06 +0700

    edit config again

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "development" : {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_DATABASE,
-    'host': process.env.DB_HOST,
-    'port': process.env.DB_PORT,
-    'dialect': 'postgres'
+  development : {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   }
   
 }
